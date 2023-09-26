f40ea2ba74b98257145cdb2d16354107bb051400
Bohdan Havran <danohavran@gmail.com>
2023-09-26 11:19:34 +0300

    Configuration Management Ansible

--- Week_4-Configuration-Management-with-Ansible/variables.tf
+++ Week_4-Configuration-Management-with-Ansible/variables.tf
@@ -12,3 +12,13 @@ variable "db_password" {
   description = "Password database"
   default     = "Passw0rd"
 }
+
+variable "public_key" {
+  description = "Source public key"
+  default     = "~/.ssh/id_rsa.pub"
+}
+
+variable "private_key" {
+  description = "Source private key"
+  default     = "~/.ssh/id_rsa"
+}
